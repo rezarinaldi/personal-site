11225be21374fba6da6ad8d648add0146ba83ff7
rezarinaldi <rez4rinaldi@gmail.com>
2024-01-22 05:08:47 +0700

    style(font): update text-body costum

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
       fontFamily: {
         display: ["Inter", "sans-serif"],
       },
+      fontSize: {
+        "body-costum": [
+          "0.9375rem",
+          {
+            lineHeight: "1.625rem",
+            letterSpacing: "0.00938rem",
+            fontWeight: "500",
+          },
+        ],
+      },
+      transitionProperty: {
+        width: "width",
+        height: "height",
+        spacing: "margin, padding",
+      },
     },
   },
   plugins: [],
